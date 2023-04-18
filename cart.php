b6448abcbd7b7c000181e519e3a15972e05b5952
Mike Xiong <xiong.tchylon@gmail.com>
2023-04-18 13:34:20 +0200

    first commit

--- cart.php
+++ cart.php
@@ -1,7 +1,19 @@
-<?php require 'inc/head.php'; ?>
+<?php
+
+session_start();
+
+require 'inc/head.php';
+require 'inc/data/products.php';
+
+?>
 <section class="cookies container-fluid">
     <div class="row">
-        TODO : Display shopping cart items from $_SESSION here.
+        <!-- TODO : Display shopping cart items from $_SESSION here. -->
+        <ul>
+            <?php foreach ($_SESSION['cookies'] as $cookie) : ?>
+                <li><?= $cookie ?></li>
+            <?php endforeach ?>
+        </ul>
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
--- index.php
+++ index.php
@@ -1,5 +1,32 @@
-<?php require 'inc/data/products.php'; ?>
-<?php require 'inc/head.php'; ?>
+<?php
+
+session_start();
+
+require 'inc/data/products.php';
+require 'inc/head.php';
+
+// vérifier si GET est définie
+if (isset($_GET['add_to_cart'])) {
+
+    // vérifier si SESSION possède un tableau 'cookies', si non, créer là
+    if (!isset($_SESSION['cookies'])) {
+        $_SESSION['cookies'] = array();
+    }
+
+    // récupération de l'ID des cookies
+    $id = $_GET['add_to_cart'];
+
+    // ajouter les cookies dans la variable super globale 'cookies'
+    array_push($_SESSION['cookies'], $catalog[$id]['name']);
+}
+
+var_dump($_SESSION['cookies']);
+var_dump($catalog[$id]['name']);
+
+// session_destroy();
+// header('Location: /');
+
+?>
 <section class="cookies container-fluid">
     <div class="row">
         <?php foreach ($catalog as $id => $cookie) { ?>
@@ -7,7 +34,8 @@
                 <figure class="thumbnail text-center">
                     <img src="assets/img/product-<?= $id; ?>.jpg" alt="<?= $cookie['name']; ?>" class="img-responsive">
                     <figcaption class="caption">
-                        <h3><?= $cookie['name']; ?></h3>
+                        <h3><?= $_SESSION['cookieName'] = $cookie['name']; ?></h3>
+
                         <p><?= $cookie['description']; ?></p>
                         <a href="?add_to_cart=<?= $id; ?>" class="btn btn-primary">
                             <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
